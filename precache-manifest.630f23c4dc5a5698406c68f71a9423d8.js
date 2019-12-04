99252b550aa83ee829aac57a49fa2067e364c58a
pauljay611 <pauljay611@gmail.com>
2019-12-04 16:53:29 +0800

    Updates

---
+++ precache-manifest.630f23c4dc5a5698406c68f71a9423d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41964f0302bbf4ae49cc26438a2f38aa",
+    "url": "/react-restaurant-app/index.html"
+  },
+  {
+    "revision": "0522a39ccfc4480b1283",
+    "url": "/react-restaurant-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "78f5eb4e22e870ce31e3",
+    "url": "/react-restaurant-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0522a39ccfc4480b1283",
+    "url": "/react-restaurant-app/static/js/2.fbb6466d.chunk.js"
+  },
+  {
+    "revision": "78f5eb4e22e870ce31e3",
+    "url": "/react-restaurant-app/static/js/main.79f89a5b.chunk.js"
+  },
+  {
+    "revision": "b183c3d7594780c0d07d",
+    "url": "/react-restaurant-app/static/js/runtime-main.0755b430.js"
+  }
+]);
